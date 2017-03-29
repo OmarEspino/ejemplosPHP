290282dee7566aab5f55bafe029295e75a92cfc8
OmarEspino <omar.guma@gmail.com>
2017-03-29 12:58:59 -0600

    Add files via upload

---
+++ hola.php
@@ -0,0 +1,10 @@
+
+
+<?php
+
+    //este es un script de php
+    #este es un comentario estilo perl
+
+echo '<h1>Hola PHP</h1>';
+
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>
+        <?php 
+            #Variables; TODAS las variables se declaran con $
+            $num1 = 10;
+            $num2 = 20;
+            $suma = $num1 + $num2;
+            //echo 'La suma es $suma'; 
+            //echo "La suma es $suma";
+
+            //la concatenacion de cadenas se hace con el punto
+            echo 'La suma es '.$suma;  
+        ?>
+    </h1>
+
+</body>
+</html>
